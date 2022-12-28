38e61750090e84063c1beea43083c70f298f5710
rsnr-code <rishadnero@gmail.com>
2022-12-28 14:30:59 -0500

    created Quotes component

--- src/components/Quote.jsx
+++ src/components/Quote.jsx
@@ -1,85 +1,89 @@
-import { useEffect, useState } from "react";
-import AnimatedText from 'react-animated-text-content';
+import AnimatedText from "react-animated-text-content";
+import { useState, useEffect } from "react";
 
-const Quote = () => {
+const Quote = (props) => {
     const [quote, setQuote] = useState("");
     const [author, setAuthor] = useState("");
-  
+    const [newQuote, setNewQuote] = useState(false);
+
+    const { quoteRequest } = props;
+
     useEffect(() => {
-     const clickHandler = async () => {
-      await fetch("https://type.fit/api/quotes")
-        .then(function (response) {
-          return response.json();
-        })
-        .then(function (data) {
-          console.log(data);
-          const quotes = data;
-          const quote = quotes[Math.floor(Math.random() * quotes.length)];
-          console.log(quote);
-          setQuote(quote.text);
-          setAuthor(quote.author);
-        });
-    };   
-    clickHandler();
-    }, [])
+        const clickHandler = async () => {
+          await fetch("https://type.fit/api/quotes")
+            .then(function (response) {
+              return response.json();
+            })
+            .then(function (data) {
+              const quotes = data;
+              const quote = quotes[Math.floor(Math.random() * quotes.length)];
     
+              setQuote(quote.text);
+              setAuthor(quote.author);
+            });
+        };
+        clickHandler();
+      }, [newQuote]);
+
+      const newQuoteHandler = () => {
+        setNewQuote(!newQuote);
+      };
 
     return ( 
-        <section id="showcase">
-          <header>
-            <h2 className="logo">QUOTES TELEVISION</h2>
-          </header>
-          <video src="/slowtv.mp4" muted loop autoPlay playsInline></video>
-          <div className="overlay"></div>
-          <div className="text">
+        <>
+        <video src="/slowtv.mp4" muted loop autoPlay playsInline></video>
+        <div className="overlay"></div>
+        <div className="text">
+          <h2>
+            <AnimatedText
+              type="words" // animate words or chars
+              animation={{
+                x: "200px",
+                y: "-20px",
+                scale: 5,
+                ease: "ease-in-out",
+              }}
+              animationType="lights"
+              interval={0.04}
+              duration={0.5}
+              tag="p"
+              className="animated-paragraph"
+              includeWhiteSpaces
+              threshold={0.1}
+              rootMargin="20%"
+            >
+              {quote}
+            </AnimatedText>
+          </h2>
 
-            <h2>
-          <AnimatedText
-  type="words" // animate words or chars
-  animation={{
-    x: '200px',
-    y: '-20px',
-    scale: 5,
-    ease: 'ease-in-out',
-  }}
-  animationType="blocks "
-  interval={0.04}
-  duration={0.5}
-  tag="p"
-  className="animated-paragraph"
-  includeWhiteSpaces
-  threshold={0.1}
-  rootMargin="20%"
->
-  {quote}
-</AnimatedText>
-</h2>
-           
-<p>
-<AnimatedText
-  type="words" // animate words or chars
-  animation={{
-    x: '200px',
-    y: '-20px',
-    scale: 5,
-    ease: 'ease-in-out',
-  }}
-  animationType="blocks "
-  interval={0.04}
-  duration={0.5}
-  tag="p"
-  className="animated-paragraph"
-  includeWhiteSpaces
-  threshold={0.1}
-  rootMargin="20%"
->
-  {author}
-</AnimatedText>
-</p>
-            <p></p>
-            <a href="/">Go Back</a>
+          <p>
+            <AnimatedText
+              type="words" // animate words or chars
+              animation={{
+                x: "200px",
+                y: "-20px",
+                scale: 5,
+                ease: "ease-in-out",
+              }}
+              animationType="lights"
+              interval={0.04}
+              duration={0.5}
+              tag="p"
+              className="animated-paragraph"
+              includeWhiteSpaces
+              threshold={0.1}
+              rootMargin="20%"
+            >
+              {author}
+            </AnimatedText>
+          </p>
+
+          <div className="quote">
+            <a onClick={newQuoteHandler}>New Quote</a>
+            <a onClick={quoteRequest}>Go Back</a>
           </div>
-        </section>
+        </div>
+      </>
      );
 }
  
